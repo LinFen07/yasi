5f5ffd390e96a0bbbaf4ba3677bb25cdbaeb11c5
LinFen07 <linlc2007@qq.com>
2026-08-17 23:05:23 +0800

    ci: relax lint rules, fix diff-check

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,13 @@ module.exports = {
   root: true,
   extends: ['react-app'],
   rules: {
-    'no-unused-vars': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
     'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'warn',
+    'eqeqeq': 'warn',
+    'jsx-a11y/anchor-is-valid': 'warn',
+    'jsx-a11y/alt-text': 'warn',
+    'import/no-anonymous-default-export': 'off',
   },
 };
